6d7dd375b3413eb70359fa6c6e0677ed7a372e16
James P Early, Ph.D <james.early@oswego.edu>
2023-05-17 13:41:35 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    scene.setBackgroundColor(7)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    scene.setBackgroundColor(5)
+})
+scene.setBackgroundColor(1)
